fd40f3aa16f2770e931c04bc106b5dde4e6ac8f2
MaiaPelletier <maia.pelletier15@gmail.com>
2023-02-06 11:05:41 -0500

    Add 8.5.1 and 8.5.2

--- _translation_script.R
+++ _translation_script.R
@@ -5,8 +5,8 @@ library(dplyr)
 library(stringr)
 library(readr)
 
-en <- get_cansim("46-10-0065-01", factors = FALSE, language = "en")
-fr <- get_cansim("46-10-0065-01", factors = FALSE, language = "fr")
+en <- get_cansim("14-10-0417-01", factors = FALSE, language = "en")
+fr <- get_cansim("14-10-0417-01", factors = FALSE, language = "fr")
 
 dfs <- list(en, fr)
 
@@ -17,7 +17,7 @@ filtering_dfs <- function(df) {
   df %>%
     # Need to change these selections to the disaggregations to be translated
     # and the respective hierarchy column
-    select(c(4, 20)) %>%
+    select(c(6, 27)) %>%
     distinct() %>%
     # filter_at(c(2), ~ str_starts(.x, "1.2.3.34")) %>%
     # mutate_at(c(1), ~ str_remove(.x, " \\[.*\\]")) %>%
