ea22ec42f5d0cf6273a656f3c0896b76e260cf30
MaiaPelletier <maia.pelletier15@gmail.com>
2023-02-21 09:54:06 -0500

    modify 3.4.1

--- GIF/R/indicator_3-4-1.R
+++ GIF/R/indicator_3-4-1.R
@@ -10,7 +10,6 @@ library(readr)
 
 # get data from stc api
 mortality_data <- get_cansim("13-10-0394-01", factors = FALSE)
-View(mortality_data)
 
 # diseases considered in this indicator
 diseases <- c(
@@ -23,56 +22,25 @@ diseases <- c(
   "Chronic lower respiratory diseases [J40-J47]"
 )
 
-# ages considered in this indicator
-ages <- c(
-  "Age at time of death, 30 to 34 years",
-  "Age at time of death, 35 to 44 years",
-  "Age at time of death, 45 to 54 years",
-  "Age at time of death, 55 to 64 years",
-  "Age at time of death, 65 to 74 years",
-  "Age at time of death, 75 to 84 years",
-  "Age at time of death, 85 and over"
-)
-
 
 data_final <- 
   mortality_data %>%
   filter(
     REF_DATE >= 2015,
-    UOM == "Percentage",
-    Characteristics == "Percentage of deaths",
-    `Age at time of death` %in% ages,
+    Characteristics == "Age-specific mortality rate per 100,000 population",
     `Leading causes of death (ICD-10)` %in% diseases 
-    ) %>%
+  ) %>% 
   mutate(
-    GEO = str_remove(GEO, ", place of residence"),
     `Age at time of death` = str_remove(`Age at time of death`, "Age at time of death, "),
+    `Age at time of death` = str_to_sentence(`Age at time of death`),
     `Leading causes of death (ICD-10)` = str_remove(`Leading causes of death (ICD-10)`, " \\[.*\\]")
   ) %>%
   select(
     Year = REF_DATE, 
-    Geography = GEO, 
     `Age at time of death`, 
     Sex, 
     `Causes of death` = `Leading causes of death (ICD-10)`, 
     Value = VALUE
-    )
+  )
 
-write_csv(data_final, here("gif-data-processing", "data", "indicator_3-4-1.csv"))
-
-# total_line <-
-#   mortality_data %>%
-#   filter(
-#     Geography == "Canada", 
-#     Sex == "Both sexes"
-#     )
-#   mutate_at(c("Geography", "Sex"), ~ "")
-
-# data_final <-
-#   bind_rows(
-#     total_line,
-#     suicide_mort_rate
-#     # suicide_mort_rate %>%
-#     #   filter(!(Geography == "Canada" & Sex == "Both sexes"))
-#   ) %>%
-#   filter(!str_starts(Geography, "Unknown"))
+write_csv(data_final, here("data", "indicator_3-4-1.csv"))
